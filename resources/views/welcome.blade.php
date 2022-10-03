241b5bd98535eaa1c2b51c7ee93e1a9daf216366
unknown <fernandoshaggy562@gmail.com>
2022-10-02 19:23:16 -0500

    cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Laravel</title>
+    <title>Bienvenido</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -37,65 +37,62 @@
             background-color: white
         }
         .reg{
-
   border-radius: 15px;
   background: rgb(53, 184, 21);
   color: white;
   padding: 20px 46%;
   text-decoration: none;
-
-        }
-        .x{
-    text-align: center;position: relative;
-    top: 50%;
-    transform: translateY(50%);
-    -webkit-transform: translateY(50%);
-    -ms-transform: translateY(50%);
         }
 
-    </style>
+
+   </style>
+
 </head>
-<div id="particles-js"></div>
+<div id="particles-js"  style="width: 100%; height: 100vh; position: fixed; z-index: -1;"></div>
 <body>
 
-    <div class="container-fluid fixed-top p-4">
-        <div class="col-12">
-            <div class="d-flex justify-content-end">
-                @if (Route::has('login'))
-                    <div class="">
-                        @auth
-                    <!--  <a href="{{ url('/dashboard') }}" class="text-muted">Dashboard</a> -->
-                        @else
-                        <a href="#" class="but2"> Centro de Ayuda</a>
-                            <a href="{{ route('login') }}" class="ms-4 but">Iniciar sesion</a>
+<header>
+    <div class="col-12">
+        <div class="d-flex justify-content-end" style="margin-top:20px">
+            @if (Route::has('login'))
+                <div class="">
+                    @auth
+                <!--  <a href="{{ url('/dashboard') }}" class="text-muted">Dashboard</a> -->
+                    @else
+                    <a href="#" class="but2"> Centro de Ayuda</a>
+                        <a href="{{ route('login') }}" class="ms-4 but">Iniciar sesion</a>
 
-                            @if (Route::has('register'))
-                <!-- <a href="{{ route('register') }}" class="ms-4 text-muted">Register</a> -->
-                            @endif
+                        @if (Route::has('register'))
+            <!-- <a href="{{ route('register') }}" class="ms-4 text-muted">Register</a> -->
                         @endif
-                    </div>
-                @endif
-            </div>
-
-            <div class="row">
-            <div class="col-md-6 x">
-            <div style="position: relative; float: left;">
-                <img src="{{asset('img/LogoT.png')}}" alt="logo"  width="600">
-            </div>
-            <h1 class="let">Te damos la bienvenida</h1>
-            <hr>
-            <br>
-            <div>
-                <a href="{{ route('register') }}" class="reg">Register</a>
-            </div>
-        </div>
-        </div>
-
-
+                    @endif
+                </div>
+            @endif
         </div>
     </div>
+</header>
+<br>
+<div class="row">
+    <div class="col-md-6">
+<img src="{{asset('img/LogoT.png')}}" class="img-fluid" alt="logo">
+<h1 class="let">Te damos la bienvenida</h1>
+<hr>
+<br>
+<a href="{{ route('register') }}" class="reg">Register</a>
+</div>
+
+    <div class="col-md-6">
+        <br>
+
+    </div>
+
+
+</div>
+
+
 
 </body>
 </html>
 <script src="js/particles.min.js"></script>
 <script src="js/particulass.js"></script>
+
