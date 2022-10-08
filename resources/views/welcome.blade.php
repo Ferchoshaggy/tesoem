290f16527ed8f0d72897daedc130e72de9f11b00
unknown <fernandoshaggy562@gmail.com>
2022-10-08 00:05:31 -0500

    imagenes slider

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <style>
+
         body {
             font-family: 'arial';
             background: #193333;
@@ -40,9 +41,38 @@
   border-radius: 15px;
   background: rgb(53, 184, 21);
   color: white;
-  padding: 20px 46%;
+  padding: 20px 39%;
   text-decoration: none;
+  font-size: 40px;
+  font-weight: bold;
         }
+
+/* carusel*/
+
+.container-slider{
+ width: 90%;
+ max-width: 900px;
+ margin: auto;
+ overflow: hidden;
+ position: relative;
+ border-radius: 10px;
+}
+.slider{
+display: flex;
+width: 400%; /*Modificar si quiero agregar mas imagenes 100% por imagen*/
+height: 400px;
+margin-left: -100%;
+}
+.slider__section{
+width: 100%;
+}
+.slider__img{
+display: block;
+width: 100%;
+height: 100%;
+object-fit: cover;
+}
+
 
 
    </style>
@@ -74,16 +104,34 @@
 <br>
 <div class="row">
     <div class="col-md-6">
+<div style="margin-top:90px">
 <img src="{{asset('img/LogoT.png')}}" class="img-fluid" alt="logo">
 <h1 class="let">Te damos la bienvenida</h1>
 <hr>
 <br>
-<a href="{{ route('register') }}" class="reg">Register</a>
+<a href="{{ route('register') }}" class="reg">Registrate</a>
+</div>
 </div>
 
     <div class="col-md-6">
         <br>
+<div class="container-slider" style="margin-top: 40px">
+    <div class="slider" id="slider">
+      <div class="slider__section">
+        <img src="{{asset('img/C1.png')}}" alt="" class="slider__img">
+    </div>
+      <div class="slider__section">
+        <img src="{{asset('img/C2.png')}}" alt="" class="slider__img">
+    </div>
+      <div class="slider__section">
+        <img src="{{asset('img/C3.png')}}" alt="" class="slider__img">
+    </div>
+      <div class="slider__section">
+        <img src="{{asset('img/C4.jpg')}}" alt="" class="slider__img">
+    </div>
+    </div>
 
+</div>
     </div>
 
 
@@ -96,3 +144,42 @@
 <script src="js/particles.min.js"></script>
 <script src="js/particulass.js"></script>
 
+<script>
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+
+function Next(){
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft="-200%";
+    slider.style.transition="all 0.5s";
+    setTimeout(function(){
+        slider.style.transition="none";
+        slider.insertAdjacentElement('beforeend',sliderSectionFirst);
+        slider.style.marginLeft="-100%";
+    }, 500);
+}
+
+function Prev(){
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginLeft="0";
+    slider.style.transition="all 0.5s";
+    setTimeout(function(){
+        slider.style.transition="none";
+        slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+        slider.style.marginLeft="-100%";
+    }, 500);
+}
+
+
+setInterval(function(){
+Next();
+}, 3000);
+
+</script>
